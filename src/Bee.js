265766779ac4a6d51d1a0fcf9812188eb88f92c5
Marketgai <marcusalee@gmail.com>
2020-10-15 22:59:57 -0700

    first try with no tests

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,6 @@
 class Bee {
-  // TODO..
+  constructor(name) {
+    this.
+  }
+
 };
--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,15 @@
-class RetiredForagerBee {
-  // TODO..
+class RetiredForagerBee extends ForagerBee {
+  constructor () {
+    this.age = 40;
+    this.job = 'gamble';
+    this.canFly = false;
+    this.color = 'grey';
+    super();
+  }
+  forage() {
+    return 'I am too old, let me play cards instead';
+  }
+  gamble(treasure) {
+    this.treasureChest.push(treasure);
+  }
 };
